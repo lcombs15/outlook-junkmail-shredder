037332e05c3c95929874886f04e656159c02bb78
Lucas Combs <l-combs@hotmail.com>
2025-07-26 17:47:59 -0400

    better solution for deploy retry

--- buildAndDeploy.sh
+++ buildAndDeploy.sh
@@ -1,8 +1,10 @@
 sudo docker stack remove hotmail-junk-removal
 sudo docker build -t hotmail-junk-removal-tool .
 deploy () {
-	sudo docker stack deploy -c docker-compose.yml hotmail-junk-removal --detach=true || sleep 1
+	sudo docker stack deploy -c docker-compose.yml hotmail-junk-removal --detach=true
 }
 
 # For some reason the docker stack is angry sometimes
-deploy || deploy || deploy || deploy || deploy
+do
+  deploy
+done while [ ! $? -eq 0 ]
