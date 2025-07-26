75f5a905933b7db58a2163c971535b72d4cfcb48
Lucas Combs <l-combs@hotmail.com>
2025-07-26 17:44:27 -0400

    Chill out when docker is angry

--- buildAndDeploy.sh
+++ buildAndDeploy.sh
@@ -1,7 +1,7 @@
 sudo docker stack remove hotmail-junk-removal
 sudo docker build -t hotmail-junk-removal-tool .
 deploy () {
-	sudo docker stack deploy -c docker-compose.yml hotmail-junk-removal --detach=true
+	sudo docker stack deploy -c docker-compose.yml hotmail-junk-removal --detach=true || sleep 1
 }
 
 # For some reason the docker stack is angry sometimes
