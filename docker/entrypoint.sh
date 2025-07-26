75fa9ca20f1a07feaf9bc369c092cd36a3d40a8b
Lucas Combs <l-combs@hotmail.com>
2025-07-26 17:12:22 -0400

    helpful logging

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -5,11 +5,10 @@ LOG_FILE=/mnt/logs/cron.log
 
 touch $LOG_FILE
 
+echo 'Container started!' $(date)  | tee -a $LOG_FILE
+
 # Ensure cron has all the needed env
 export >> /etc/profile
-
-# Confirm timezone
-date
 
 # Run right away to auth
 npm run main | tee -a $LOG_FILE
