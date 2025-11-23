ab5759e4d87052f5763dbebe52e3612327ce3641
Lucas Combs <l-combs@hotmail.com>
2025-11-23 13:09:34 -0500

    NOGH: Patch up entrypoint

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -4,6 +4,7 @@ set -e
 LOG_FILE=/mnt/logs/cron.log
 
 touch $LOG_FILE
+cat $LOG_FILE
 
 echo 'Container started!' $(date)  >> $LOG_FILE
 
@@ -14,11 +15,11 @@ export >> /etc/profile
 npm run main | tee -a $LOG_FILE
 
 # Start cron
-echo "Starting cron service..." && cron
+echo "Starting cron service" >> $LOG_FILE
+cron || echo "Cron running already" >> $LOG_FILE
 
-echo "Starting REST api"
-{npm run api | tee -a $LOG_FILE} &
+echo "Starting REST api" >> $LOG_FILE
+npm run api >> $LOG_FILE &
 
-# Keep container running and show logs
-cat $LOG_FILE
+# Keep container running
 tail -n 0 -f $LOG_FILE
