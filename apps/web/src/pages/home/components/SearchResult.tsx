c3e4588c412826667f3694ed59a5fad1a81a9e2f
Lucas Combs <l-combs@hotmail.com>
2026-09-13 12:13:51 -0400

    NOGH: Introduce searching for shredded messages

--- apps/web/src/pages/home/components/SearchResult.tsx
+++ apps/web/src/pages/home/components/SearchResult.tsx
@@ -1,11 +1,20 @@
 import type { HistoryResult } from "~/services/history-service";
+import classNames from "classnames";
 
 export function SearchResult({ content }: { content: HistoryResult }) {
     const { fromEmail, shreddedReason, subject, receiveAt, wasShredded } =
         content;
     return (
         <a href={`/history/${content.id}`} className={"hover:underline"}>
-            <div className="flex flex-col gap-1 bg-card shadow-sm text-card-foreground p-2 rounded-md ring border hover:bg-primary">
+            <div
+                className={classNames(
+                    "shadow-sm flex flex-col gap-1 bg-card text-card-foreground p-2 rounded-md border hover:bg-primary",
+                    {
+                        "border-destructive/75": wasShredded,
+                        ring: !wasShredded,
+                    },
+                )}
+            >
                 <p className={"font-semibold"}>From: {fromEmail}</p>
                 <p className="overflow-auto">Subject: {subject}</p>
                 {wasShredded && <p>Shredded Reason: {shreddedReason}</p>}
